919adf2b7df33c66176ead840833a8c0053dbd7d
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-06-23 20:40:45 +0900

    초기 세팅

---
+++ ch6/6-1.js
@@ -0,0 +1,39 @@
+export function printOwing(invoice) {
+  // 중첩함수로 만들지 않았음.
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
+
+const invoice = {
+  orders: [{ amount: 2 }, { amount: 5 }],
+  customer: "엘리",
+};
+
+printOwing(invoice);
+
+function printDetails(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function recordDueDate(invoice) {
+  const today = new Date();
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+}
+
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
+
+function printBanner() {
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+}
