c37d265c2c8e4f8f69ada9578b1740c478768fcc
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-06-25 14:54:34 +0900

    6-6 변수 캡슐화하기

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,6 +1,6 @@
-import { getDefaultOwner } from './6-6.js';
+import { getDefaultOwner } from "./6-6.js";
 
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
+owner.firstName = "1박";
 console.log(owner);
 console.log(getDefaultOwner());
--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,14 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 
 export function getDefaultOwner() {
-  return defaultOwner;
+  return Object.assign({}, defaultOwner);
 }
+
+// 방법 1 : 객체를 복사해서 반환해주기
+// Object.assign(defaultOwner); {...defaultOwner}
+
+// 방법 2 : 클래스로 만들어주기
+
+// 내생각 : const로 만들기 : 틀림
+// let이 아닌 const로 변경하면 수정이 안될거라 판단했는데
+// object이기에 프로퍼티의 키를 바꾸는 건 안되지만 프로퍼티의 값을 바꾸는 건 가능하기에 실패
