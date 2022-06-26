0ce92e328a285656846b41b5b56236b3034f03fc
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-06-26 18:32:10 +0900

    6-11 단계쪼개기

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,38 @@
-import fs from 'fs';
+import fs from "fs";
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+// args는 실행할 파일
+// run 함수를 만들어 테스트성을 높임
+function run(args) {
+  return countOrders(parseCommandLine(args));
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function parseCommandLine(args) {
+  if (!args) {
+    throw new Error("파일 이름을 입력하세요");
+  }
+
+  const fileName = `./${args}.json`;
+
+  if (!fs.existsSync(fileName)) {
+    throw new Error("파일이 존재하지 않습니다");
+  }
+
+  const countReadOnly = args.includes("-r");
+
+  return {
+    fileName,
+    countReadOnly,
+  };
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countOrders({ fileName, countReadOnly }) {
+  const orders = JSON.parse(fs.readFileSync(fileName));
+  // node 6-11-2 -r 옵션을 주면 ready인 상태만 간주한다.
+  const filtered = countReadOnly
+    ? orders.filter((order) => order.status === "ready")
+    : orders;
+
+  return filtered.length;
 }
+
+run(process.argv[2]);
--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,30 +1,98 @@
-export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+// 클래스로 만들기
+
+// 고민1 : product를 애초에 객체로 받아올지
+// 일단 객체로 받아서 쓰자
+class Order {
+  #quantity;
+  #product;
+  #shippingMethod;
+  constructor(product, quantity, shippingMethod) {
+    this.#quantity = quantity;
+    this.#product = product;
+    this.#shippingMethod = shippingMethod;
+  }
+
+  get basePrice() {
+    return this.#product.basePrice * this.#quantity;
+  }
+
+  get discount() {
+    return (
+      Math.max(this.#quantity - this.#product.discountThreshold, 0) *
+      this.#product.basePrice *
+      this.#product.discountRate
+    );
+  }
+
+  get shippingPerCase() {
+    return this.basePrice > this.#shippingMethod.discountThreshold
+      ? this.#shippingMethod.discountedFee
+      : this.#shippingMethod.feePerCase;
+  }
+
+  get shippingCost() {
+    return this.#quantity * this.shippingPerCase;
+  }
+
+  get price() {
+    return this.basePrice - this.discount + this.shippingCost;
+  }
 }
 
+class Product {
+  #basePrice;
+  #discountRate;
+  #discountThreshold;
+  constructor(data) {
+    this.#basePrice = data.basePrice;
+    this.#discountRate = data.discountRate;
+    this.#discountThreshold = data.discountThreshold;
+  }
+  get basePrice() {
+    return this.#basePrice;
+  }
+  get discountRate() {
+    return this.#discountRate;
+  }
+  get discountThreshold() {
+    return this.#discountThreshold;
+  }
+}
+class ShippingMethod {
+  #discountThreshold;
+  #feePerCase;
+  #discountedFee;
+
+  constructor(data) {
+    this.#discountThreshold = data.discountThreshold;
+    this.#feePerCase = data.feePerCase;
+    this.#discountedFee = data.discountedFee;
+  }
+
+  get discountThreshold() {
+    return this.#discountThreshold;
+  }
+
+  get feePerCase() {
+    return this.#feePerCase;
+  }
+
+  get discountedFee() {
+    return this.#discountedFee;
+  }
+}
 // 사용 예:
-const product = {
+const product = new Product({
   basePrice: 10,
   discountRate: 0.1,
   discountThreshold: 10,
-};
+});
 
-const shippingMethod = {
+const shippingMethod = new ShippingMethod({
   discountThreshold: 20,
   feePerCase: 5,
   discountedFee: 3,
-};
+});
 
-const price = priceOrder(product, 5, shippingMethod);
-console.log(price);
+const price = new Order(product, 5, shippingMethod);
+console.log(price.price);
