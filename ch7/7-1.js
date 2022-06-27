9257f59c108ac24b93fa6fc7701dea1c88f85091
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-06-27 12:43:17 +0900

    7-1 레코드 쪼개기 문제점

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,8 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+// 기존의 코드는 너무 쉽게 변경할 수 있음
+// 불변성을 지키지 못해 side Effect가 발생할 가능성이 높다.
+const organization = { name: "Acme Gooseberries", country: "GB" };
 
-organization.name = 'Dream Coding';
+organization.name = "Dream Coding";
+
 console.log(organization.name);
 console.log(organization.country);
