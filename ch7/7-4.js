f1a064e8e8dbe4e713fa6ab2b5cf2f1d826196e0
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-06-28 21:55:01 +0900

    7-4 임시 변수를 질의함수로 바꾸기

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -6,10 +6,18 @@ class Order {
     this.#item = item;
   }
 
+  get basePrice() {
+    return this.#quantity * this.#item;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    let discountFactor = 0.98;
+    this.basePrice > 1000 ? discountFactor : discountFactor - 0.03;
+    return this.basePrice * discountFactor;
   }
 }
+
+const qwe = new Order(300, 4);
+
+console.log(qwe.basePrice);
+console.log(qwe.price);
