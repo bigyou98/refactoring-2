900e806e82fc02ce1e3529d0148f76ad32284a4a
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-07-02 14:41:06 +0900

    8-6 문장 슬라이드하기

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,18 +1,22 @@
+// 문장 슬라이드 하기
+// 변수는 사용하는 곳과 최대한 가까이 두기
+// 그래야 읽을 때 수월하다
+
 // 예제 1
 const pricingPlan = retrievePricingPlan();
+const chargePerUnit = pricingPlan.unit;
 const order = retreiveOrder();
 let charge;
-const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
+// 조건문과 상관없는 공통 행위는 조건문 밖으로 위치한다
+
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop();
+  allocatedResources.push(result);
+
   return result;
 }
