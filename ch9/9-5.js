57b5488d8cf5f3b66addd857c58414c0b6263b02
sangkoon <bigyou00@gmail.com>
2022-07-04 20:39:39 +0900

    9-5 값을 참조로 바꾸기

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,19 @@
+// 9-5 값을 참조로 바꾸기
+// 불변성은 실수로 내가 여기저기서 값을 막 변경해도 상관이 없으니 좋다
+// 하지만 내가 변경한 것이 다른 곳에서 업데이트가 되야한다면 참조 값을 갖는 것이 좋다.
+
+// 고유한 id값을 사용하고 싶다면 repository 패턴을 사용하는 것이 좋다.
+const customerRepository = new CustomerRepository();
+
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+);
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -10,11 +22,31 @@ class Order {
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id;
+    this._name = name;
   }
 
   get id() {
     return this._id;
+  }
+}
+
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
   }
 }
