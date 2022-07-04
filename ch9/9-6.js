def192de2c2fdf59ff51004fe09ebf1ecd268050
sangkoon <bigyou00@gmail.com>
2022-07-04 20:42:17 +0900

    9-6 매직 리터럴 바꾸기

--- ch9/9-6.js
+++ ch9/9-6.js
@@ -1,3 +1,8 @@
+// 9-6 매직 리터럴 바꾸기
+
+// 의미 없는 숫자 ex) 9.81이 딱 나오면 뭔지도 모르겠고 기분만 나쁘다!
+// 의미가 있는 이름을 갖게 만들어라
 function potentialEnergy(mass, height) {
-  return mass * 9.81 * height;
+  const STANDARD_GRAVITY = 9.81;
+  return mass * STANDARD_GRAVITY * height;
 }
