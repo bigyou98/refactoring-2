580f48dcd9df3caaa2dd5faf5e13e691db5b43a8
sangkoon <bigyou00@gmail.com>
2022-07-04 20:18:46 +0900

    9-3 파생변수를 질의 함수로 바꾸기

--- ch9/9-3.js
+++ ch9/9-3.js
@@ -1,13 +1,15 @@
+// 9-3 파생변수를 질의 함수로 바꾸기
+
 // 예제 1
+// 기존의 코드는 다른 값도 같이 변경이 되어 SRP가 지켜지지 못함
 class Order {
   // 다른 코드 있다고 가정
   get discountedTotal() {
-    return this._discountedTotal;
+    // 해당 값을 사용할 때 필요한 계산이 되도록 해야한다.
+    return this._basePrice - this._discount;
   }
   set discount(value) {
-    const old = this._discount;
     this._discount = value;
-    this._discountedTotal += old - value;
   }
 }
 
@@ -15,10 +17,9 @@ class Order {
 class ProductionPlan {
   // 다른 코드 있다고 가정
   get production() {
-    return this._production;
+    return this.adjustment.reduce((sum, a) => sum + a.amount, 0);
   }
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
   }
 }
