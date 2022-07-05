8a0efdc92a6aeec97e6c2459e4fb83cceffdf836
sangkoon <bigyou00@gmail.com>
2022-07-05 14:45:31 +0900

    중첩 조건문을 보호 구문으로 바꾸기

--- ch10/10-3-1.js
+++ ch10/10-3-1.js
@@ -1,22 +1,26 @@
+// 중첩 조건문을 보호 구문으로 바꾸기
+// 중첩 조건문은 보기도 어렵고 더러워지고 유지보수하기도 어렵다
+
+// 쉽게 결과가 도출될 수 있는 부분을 앞에 배치한다.
+// 함수가 빨리 종료될 수 있도록 한다.
+// 긴 작업은 뒤에 배치한다
 export function payAmount(employee) {
-  let result;
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return { amount: 0, reasonCode: "SEP" };
   }
-  return result;
+
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: "RET" };
+  }
+
+  // 중요한 로직들
+  // lorem.ipsum(dolor.sitAmet);
+  // consectetur(adipiscing).elit();
+  // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  // ut.enim.ad(minim.veniam);
+  return someFinalComputation();
 }
 
 function someFinalComputation() {
-  return { amount: 999, reasonCode: 'UNICORN' };
+  return { amount: 999, reasonCode: "UNICORN" };
 }
--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,16 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (!isEligibleForAdjustCapital(instrument)) {
+    return 0;
   }
-  return result;
+  return (
+    (instrument.income / instrument.duration) * anInstrument.adjustmentFactor
+  );
+}
+
+function isEligibleForAdjustCapital(instrument) {
+  return (
+    instrument.capital > 0 &&
+    instrument.interestRate > 0 &&
+    instrument.duration > 0
+  );
 }
