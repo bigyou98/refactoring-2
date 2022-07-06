3c4a9784f3f4dee2b3589327ab7620611da4f04e
sangkoon <bigyou00@gmail.com>
2022-07-06 20:42:44 +0900

    10-5 특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,20 +1,31 @@
+// 특이 케이스 추가하기
+// 특이한게 있다면 기존의 class를 상속해서 특이class로 만들어줘라
+// null이라는 원시값을 사용하는 것이 아닌 null 객체를 만들어줘라
+
+// 유지보수와 확장성이 향상됨
 export class Site {
+  #customer;
+
   constructor(customer) {
-    this._customer = customer;
+    this.#customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this.#customer === "unknown"
+      ? new UnknownCustomer()
+      : new Customer(this.#customer);
   }
 }
 
 export class Customer {
+  #name;
+
   constructor(name) {
-    this._name = name;
+    this.#name = name;
   }
 
   get name() {
-    return this._name;
+    return this.#name;
   }
 
   get billingPlan() {
@@ -30,13 +41,19 @@ export class Customer {
   }
 }
 
+class UnknownCustomer extends Customer {
+  get name() {
+    return "unknown";
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
-  // 더 많은 코드가 여기에 있음
+
   let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
+
+  customerName = aCustomer.name;
 
   return customerName;
 }
