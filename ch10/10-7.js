a940f703555e9aeea700d294c5a6a31dad57ee50
sangkoon <bigyou00@gmail.com>
2022-07-06 20:51:38 +0900

    제어 플래그를 탈출문으로 바꾸기

--- ch10/10-7.js
+++ ch10/10-7.js
@@ -1,8 +1,9 @@
+// 제어 플래그를 탈출문으로 바꾸기
+// 제어 플래그 boolean 값으로 판단하는 것이 아닌 언어에서 제공하는 break나 continue를 사용해라
+
 for (const p of people) {
-  if (!found) {
-    if (p === 'Don') {
-      sendAlert();
-      found = true;
-    }
+  if (p === "Don") {
+    sendAlert();
+    break;
   }
 }
