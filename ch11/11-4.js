8f6b488a8641576c7f858ed4125d04516cdac33b
sangkoon <bigyou00@gmail.com>
2022-07-07 20:24:24 +0900

    객체 통째로 넘기기

--- ch11/11-4.js
+++ ch11/11-4.js
@@ -1,9 +1,13 @@
+// 객체 통째로 넘기기
+
+// 인자로 data를 넘길때 어느정도 크기로 넘길까...
+// 객체 자체를 줄수도 있고
+// 객체의 2가지만 필요한데 객체 전체를 받을 필요는 없기에 인자로 2개만 받아온다.
+
 export function temperatureAlerts(room, plan) {
   const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
-    alerts.push('room temperature went outside range');
+  if (!plan.withinRange(room.daysTempRange)) {
+    alerts.push("room temperature went outside range");
   }
 
   return alerts;
@@ -14,9 +18,10 @@ export class HeatingPlan {
     this._temperatureRange = temperatureRange;
   }
 
-  withinRange(bottom, top) {
+  withinRange(range) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      range.bottom >= this._temperatureRange.low &&
+      range.top <= this._temperatureRange.high
     );
   }
 }
