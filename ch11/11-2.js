ff97afe80dc314ec5306363bd8a62e2871f5b6a6
sangkoon <bigyou00@gmail.com>
2022-07-07 20:05:03 +0900

    함수 매개변수화 하기

--- ch11/11-2.js
+++ ch11/11-2.js
@@ -1,36 +1,41 @@
-// 예제 1
-function tenPercentRaise(person) {
-  person.salary = person.salary.multiply(1.1);
-}
+// 함수 매개변수화 하기
+// 공통된 로직은 뽑아내고 살짝 다른 부분은 매개변수로 전달해라!
 
-function fivePercentRaise(person) {
-  person.salary = person.salary.multiply(1.05);
+// 예제 1
+function raise(person, factor) {
+  person.salary = person.salary.multiply(1 + factor);
 }
 
 // 예제 2
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    band(usage, bottom) * 0.03 +
+    band(usage, middle) * 0.05 +
+    band(usage, top) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
-}
+function band(usage, type) {
+  switch (type) {
+    case "bottom":
+      return Math.min(usage, 100);
 
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
+    case "middle":
+      return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
+    case "top":
+      return usage > 200 ? usage - 200 : 0;
+
+    default:
+      break;
+  }
 }
 
 function usd(value) {
   return {
-    currency: '$',
-    currencyName: 'USD',
+    currency: "$",
+    currencyName: "USD",
     value: value,
   };
 }
