a307bf4c24ee9bccf771da220da9450494332436
sangkoon <bigyou00@gmail.com>
2022-07-07 19:52:48 +0900

    질의 함수와 변경 함수 분리하기

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,32 @@
+// 질의 함수와 변경 함수 분리하기
+
+// 한가지 일만 해라~~!!!
 // 예제 1
-function totalOutstandingAndSendBill() {
+function totalOutstanding() {
   const result = customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
-  sendBill();
   return result;
+}
+function sendBill() {
+  // bill을 보냄
 }
 
 // 예제 2
+// 악당을 찾는 로직
+// 알람을 보내는 로직
+// 위 2가지 일을 1번에 처리하려고 한다.
 export function alertForMiscreant(people, alarm) {
-  for (const p of people) {
-    if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
-    }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
-    }
-  }
-  return '';
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant(people) {
+  const villain = ["Don", "John"];
+  return people.find((p) => villain.includes(p));
 }
 
 function setOffAlarms(alarm, p) {
-  alarm.setOff('Found Miscreant ' + p);
+  alarm.setOff("Found Miscreant " + p);
 }
