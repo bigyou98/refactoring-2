f3043d0037df51104b567f2f64744e3fdc5310db
sangkoon <bigyou00@gmail.com>
2022-07-08 16:17:15 +0900

    명령을 함수로 바꾸기

--- ch11/11-10.js
+++ ch11/11-10.js
@@ -1,13 +1,18 @@
+// 명령을 함수로 바꾸기
+
+// 계속 값을 갖고 있어야 한다면 프로퍼티로 만들어 두자
+// 계속 값을 가질 필요가 없을 때는 함수로 만들자
 export class ChargeCalculator {
   constructor(customer, usage, provider) {
     this._customer = customer;
     this._usage = usage;
     this._provider = provider;
   }
+
   get baseCharge() {
     return this._customer.baseRate * this._usage;
   }
-  get charge() {
+  charge() {
     return this.baseCharge + this._provider.connectionCharge;
   }
 }
