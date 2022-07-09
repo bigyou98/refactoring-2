020606aa23f4ac0d3279caafb206cfaa4811b0ab
sangkoon <bigyou00@gmail.com>
2022-07-09 14:41:39 +0900

    오류 코드를 예외로 바꾸기

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,21 @@
+//  오류 코드를 예외로 바꾸기
+
 function localShippingRules(data) {
-  if (data) return new ShippingRules(data);
-  else return -23;
+  if (data) {
+    return new ShippingRules(data);
+  }
+  throw new OrderProcessingError(-23);
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super();
+    this.errorCode = errorCode;
+  }
+}
+
+try {
+  const result = localShippingRules();
+} catch (error) {
+  console.log(error);
 }
