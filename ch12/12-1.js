94cc410024db6fb22948720723768d9234eb7552
sangkoon <bigyou00@gmail.com>
2022-07-10 17:02:15 +0900

    메서드 올리기

--- ch12/12-1.js
+++ ch12/12-1.js
@@ -1,24 +1,17 @@
+// 메서드 올리기
+
 // 예시 1
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   get name() {}
 }
-
-class Engineer extends Employee {
-  get name() {}
-}
+class Salesperson extends Employee {}
+class Engineer extends Employee {}
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
+class Party {
   get totalAnnualCost() {
     return this.monthlyCost * 12;
   }
 }
-class Employee extends Party {
-  get annualCost() {
-    return this.monthlyCost * 12;
-  }
-}
+class Department extends Party {}
+class Employee extends Party {}
