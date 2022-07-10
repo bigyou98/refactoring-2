45ef415d5839896eb9f72af411923ae8af10b2a3
sangkoon <bigyou00@gmail.com>
2022-07-10 17:09:44 +0900

    메서드 내리기

--- ch12/12-4.js
+++ ch12/12-4.js
@@ -1,6 +1,10 @@
-class Employee {
-  get quota() {}
-}
+// 메서드 내리기
+// 서브 클래스에만 속하는 기능은 서브 클래스에서만 정의해라
+// 전체적으로 공통 사용하는 함수만 부모 클래스에 올려라
+
+class Employee {}
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  get quota() {}
+}
