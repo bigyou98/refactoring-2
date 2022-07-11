18f7b57e6596c467d23f5f84787518362d765d15
sangkoon <bigyou00@gmail.com>
2022-07-11 15:08:46 +0900

    위임하기

---
+++ ch12/12-10-composition.js
@@ -0,0 +1,36 @@
+// 컴포지션(위임, 포함)
+// 상속을 하지 않고 해당 하는 것을 주입을 해주는 것이다.
+// 전달 받은 것을 사용하는 식
+// 레고처럼 조합
+
+class Printer {
+  #printerHeader;
+  constructor(printDelegate) {
+    this.#printerHeader = printDelegate;
+  }
+  print() {
+    this.#printerHeader
+      ? this.#printerHeader.print()
+      : console.log("기본적인 출력!");
+  }
+}
+
+class RedPrinterHeader {
+  print() {
+    console.log("빨간 출력!");
+  }
+}
+
+class BlackPrinterHeader {
+  print() {
+    console.log("빨간 출력!");
+  }
+}
+
+const printers = [
+  new Printer(),
+  new Printer(new RedPrinterHeader()),
+  new Printer(new BlackPrinterHeader()),
+];
+
+printers.forEach((printer) => printer.print());
---
+++ ch12/12-10.js
@@ -0,0 +1,14 @@
+class Printer {
+  print() {
+    console.log("기본적인 출력!");
+  }
+}
+class RedPrinter extends Printer {
+  print() {
+    console.log("빨간 출력!");
+  }
+}
+
+const printers = [new Printer(), new RedPrinter()];
+
+printers.forEach((printer) => printer.print());
